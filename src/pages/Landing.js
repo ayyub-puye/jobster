5ec29c1e5f4fccca59abfa48fb226318e13fce4b
ayyub puye <ayoub.pouye870@gmail.com>
2024-10-19 13:09:46 +0330

    5-Landing-basic-structure-readme

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,7 +3,26 @@ import main from '../assets/images/main.svg'
 
 const Landing = () => {
   return (
-    <h1>Landing page</h1>
+    <main>
+      <nav>
+        <img src={logo} alt="jobster logo" className="logo" />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Hoodie taxidermy copper mug art party, intelligentsia PBR&B vape
+            paleo fashion axe. Schlitz deep v live-edge, quinoa tousled
+            single-origin coffee prism succulents tilde
+          </p>
+          <button className="btn btn-hero">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </main>
   )
 }
 export default Landing
