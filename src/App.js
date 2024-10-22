a9ed7eccbc00f9bf5107786d77a3085959a61500
ayyub puye <ayoub.pouye870@gmail.com>
2024-10-22 22:09:23 +0330

    13-react-router-setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Dashboard, Error, Register, Landing } from './pages'
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="landing" element={<Landing />} />
+        <Route path="register" element={<Register />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import { Logo } from '../components'
@@ -18,7 +19,9 @@ const Landing = () => {
             paleo fashion axe. Schlitz deep v live-edge, quinoa tousled
             single-origin coffee prism succulents tilde
           </p>
-          <button className="btn btn-hero">Login/Register</button>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
